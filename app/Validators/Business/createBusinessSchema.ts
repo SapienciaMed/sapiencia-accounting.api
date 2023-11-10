e042653f7d94f275e3cfcbc3ec025456d90eb7a2
Esteban Arias <earias@i4digital.com>
2023-11-09 23:03:51 -0500

    add bussiness and contract documentation

--- app/Validators/Business/createBusinessSchema.ts
+++ app/Validators/Business/createBusinessSchema.ts
@@ -7,12 +7,16 @@ export const createBusinessSchema = schema.create({
     rules.minLength(4),
     rules.maxLength(300),
   ]),
-  nit: schema.string([rules.required(), rules.maxLength(15)]),
+  nit: schema.string([rules.required(), rules.trim(), rules.maxLength(15)]),
   address: schema.string([
     rules.required(),
     rules.trim(),
     rules.maxLength(100),
   ]),
-  municipalityCode: schema.string([rules.required(), rules.maxLength(10)]),
+  municipalityCode: schema.string([
+    rules.required(),
+    rules.trim(),
+    rules.maxLength(10),
+  ]),
   phone: schema.string([rules.trim(), rules.required(), rules.maxLength(10)]),
 });
